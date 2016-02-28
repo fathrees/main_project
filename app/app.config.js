3c7d1235a98abdeda0f07db7945f696c0c24dcb6
Yura <yura61388@gmail.com>
2016-02-28 22:44:20 +0200

    Change app.css, del comment in app.config.js

--- app/app.config.js
+++ app/app.config.js
@@ -110,11 +110,6 @@
                 templateUrl: "app/admin/subjects/tests/test.html",
                 controller: "TestController as test"
             })
-            //.state("admin.question", {
-            //    url: "/subjects/{subject_id}/tests/{test_id}/{question_id}",
-            //    templateUrl: "app/admin/subjects/tests/question/question.html",
-            //    controller: "QuestionController as question"
-            //})
             .state("admin.question", {
                 url: "/subjects/{subject_id:int}/tests/{test_id:int}/question/{question_id:int}",
                 templateUrl: "app/admin/subjects/tests/question/question.html",

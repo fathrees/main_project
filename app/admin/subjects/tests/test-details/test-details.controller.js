fa417c5319c25713258fab0c9d514bfe75e65df5
Vitalii Vasinkevych <vasinkevych@gmail.com>
2016-03-20 21:56:55 +0200

    added orderBy in all tables. finish with crud for tests-details, has made part of validation

--- app/admin/subjects/tests/test-details/test-details.controller.js
+++ app/admin/subjects/tests/test-details/test-details.controller.js
@@ -8,41 +8,118 @@
 
     function TestDetailsController($stateParams, testsService, subjectsService, REGEXP, MESSAGE) {
         var vm = this;
+        var usedLevel = [];
+        vm.availableLevel = [];
+
         vm.headElements = testsService.getHeaderTestDetail();
         vm.removeTestLevel = removeTestLevel;
-        vm.availableLevel;
+        vm.formCollapsed = true;
+        vm.hideForm = hideForm;
+        vm.showForm = showForm;
+        vm.saveEntity = saveEntity;
+       // vm.summaryRate = 0;
+        vm.availableTask = 0;
+        vm.onlyNumber = REGEXP.ONLY_NUMBER;
+        vm.isFormInvalid = isFormInvalid;
+
+
         vm.isNotNumber = function(value){
             return isNaN(value)
         }
-        
+
         activate();
 
 
+
+
         function activate(){
-            testsService.getTestDetails($stateParams.test_id).then(function(data){
-                console.log(data);
-                vm.list = data;
-                vm.availableLevel = testsService.getLevel (vm.list);
-                console.log(vm.availableLevel);
-
-
-            })
             testsService.getOneTest($stateParams.test_id).then(function(data){
                 vm.currentTest = data[0];
             })
+
+            testsService.getTestLevel($stateParams.test_id).then(function(data){
+                vm.list = data;
+                console.log(vm.list);
+                vm.summaryRate = 0;
+                vm.list.forEach(function(item){
+                    vm.summaryRate += (parseInt(item.tasks) * parseInt(item.rate));
+                })
+                vm.taskIsUsed = taskIsUsed();
+            })
         }
 
-        function removeTestLevel(testDetail) {
+        function hideForm() {
+            vm.formCollapsed = true;
+        }
+
+
+        /**
+         * Set ava wich available Level. If it's edit,  pushed level of edited object to array.
+         * Count and set how much tasks are aviable. If it's edit,  added tasks of edited object to array.
+         * Switch editing and adding.*/
+
+        function showForm(testLevel) {
+
+            vm.availableLevel = testsService.getLevel (vm.list);
+            vm.availableTask =  testsService.availableTasks (vm.list, vm.currentTest.tasks);
+            console.log(vm.availableTask);
+            vm.formCollapsed = false;
+            if (testLevel === undefined) {
+                vm.testLevel = {
+                    test_id: $stateParams.test_id
+                }
+            }else{
+                vm.availableLevel.push(testLevel.level);
+                vm.availableTask += parseInt(testLevel.tasks)
+                vm.testLevel = testLevel;
+            }
+        }
+
+
+        function saveEntity () {
+            testsService.saveTestLevel(vm.testLevel).then(function (data) {
+                if(data.response === "ok"){
+                    alert(MESSAGE.SAVE_SUCCSES);
+
+                } else{
+                    alert(MESSAGE.SAVE_ERROR +  " " + data.response);
+                };
+                activate();
+                vm.testLevel = {};
+                hideForm();
+            })
+        }
+
+        function removeTestLevel(testLevel) {
             if(confirm(MESSAGE.DEL_CONFIRM)) {
-                testsService.removeTestLevel(testDetail.id).then(function (res) {
+                testsService.removeTestLevel(testLevel.id).then(function (res) {
                     if (res.response === "ok") {
                         alert(MESSAGE.DEL_SUCCESS)
-                    } else if (res.response = "error 23000") {
+                    } else if (res.response === "error 23000") {
                         alert(MESSAGE.DEL_ERROR);
                     }
                     activate();
                 })
             }
+        }
+
+         /**
+          * Set add button disabled.
+          * @return {boolean} When all tasks or levels were used add button is disabled and show message*/
+
+        function taskIsUsed (){
+            return !vm.availableTask || !vm.availableLevel.length;
+        }
+
+        function summaryRate (item) {
+            arrTestDetail.forEach(function(item){
+                countOfUsedTasks += parseInt(item.tasks);
+            })
+        }
+
+        function isFormInvalid () {
+            vm.invalidform = (vm.testLevel.tasks > vm.availableTask);
+            console.log((vm.testLevel.tasks > vm.availableTask));
         }
     }
 })();
--- app/services/tests.service.js
+++ app/services/tests.service.js
@@ -8,8 +8,7 @@
 
     function testsService($http, BASE_URL, ENTITIES, ACTIONS) {
 
-        var usedLevel = [];
-        var availableLevel;
+
 
         var service = {
             getTests: getTests,
@@ -18,9 +17,11 @@
             removeTest: removeTest,
             getHeader: getHeader,
             getStatus: getStatus,
-            getTestDetails: getTestDetails,
+            getTestLevel: getTestLevel,
+            saveTestLevel: saveTestLevel,
             removeTestLevel: removeTestLevel,
             getLevel: getLevel,
+            availableTasks: availableTasks,
             getHeaderTestDetail: getHeaderTestDetail
         };
 
@@ -29,17 +30,19 @@
         function _successCallback(response) {
             return response.data;
         }
-        function _errorCallback(response) {
 
+        function _errorCallback(response) {
             return response;
         }
 
         function getTests (subjectId) {
+
             return $http.get(BASE_URL + ENTITIES.TEST + ACTIONS.GET_TEST_BY_SUBJECT + "/" + subjectId)
                 .then(_successCallback, _errorCallback);
         }
 
         function getOneTest (id) {
+
             return $http.get(BASE_URL + ENTITIES.TEST + ACTIONS.GET_ENTITIES + id)
                 .then(_successCallback, _errorCallback);
         }
@@ -91,8 +94,30 @@
 
         //ТESTS-DETAILS
 
-        function getTestDetails (test_id) {
-            return $http.get(BASE_URL + ENTITIES.TEST_DETAIL + ACTIONS.GET_TEST_DETAILS + "/" + test_id)
+        function getTestLevel (id) {
+            return $http.get(BASE_URL + ENTITIES.TEST_DETAIL + ACTIONS.GET_TEST_DETAILS + "/" + id)
+                .then(_successCallback, _errorCallback);
+        }
+
+        function saveTestLevel(testLevel) {
+            if (testLevel.id === undefined) {
+
+                return _addTestLevel(testLevel);
+            } else {
+
+                return _editTestLevel(testLevel);
+            }
+        }
+
+        function _addTestLevel (testLevel){
+
+            return $http.post(BASE_URL + ENTITIES.TEST_DETAIL + ACTIONS.ADD_ENTITY, testLevel)
+                .then(_successCallback, _errorCallback);
+        }
+
+        function _editTestLevel (testLevel) {
+
+            return $http.post(BASE_URL + ENTITIES.TEST_DETAIL + ACTIONS.EDIT_ENTITY + testLevel.id, testLevel)
                 .then(_successCallback, _errorCallback);
         }
 
@@ -102,22 +127,24 @@
                 .then(_successCallback, _errorCallback);
         }
 
+
+
         /**
          * This function filterred available level for select tag.
-         * @param {array} arrTestdetail is array of objects with properties for current level
-         * @returns {array} availableLevel is the array with available level for select tag
+         * @param {array} arrTestdetail is array of objects with properties.
+         * @returns {array} availableLevel is the array with available level in select tag.
          */
 
         function getLevel (arrTestdetail){
-            var level = []
+            var level = [];
+            var usedLevel = [];
             arrTestdetail.forEach(function(item){
                 usedLevel.push(item.level);
             });
             for (var i = 1; i <= 7; i++){
                 level.push(i);
             };
-            level.push("Рівень");
-            availableLevel = level.filter(function(itemLevel){
+           var availableLevel = level.filter(function(itemLevel){
 
                 return usedLevel.every(function (usedItem) {
                     return usedItem != itemLevel;
@@ -125,6 +152,23 @@
             });
 
             return availableLevel;
+        }
+
+        /**
+         * This function count how many tasks are available.
+         * @param {array } arrTestDetail is array of objects with properties for current level.
+         * @param {number} maxQuantytyOfTasks gets from object currentTest.
+         * @returns {number} how many tasks are available.
+         */
+
+        function availableTasks (arrTestDetail, maxQuantytyOfTasks){
+            console.log(arrTestDetail + "   " + maxQuantytyOfTasks)
+            var countOfUsedTasks = 0;
+            arrTestDetail.forEach(function(item){
+                countOfUsedTasks += parseInt(item.tasks);
+            })
+
+            return (parseInt(maxQuantytyOfTasks) - countOfUsedTasks);
         }
 
 

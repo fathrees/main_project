33b7b934a5c48dc33d22332a9891d5b8077f104f
Yura <yura61388@gmail.com>
2016-03-21 15:40:58 +0200

    refactor code and add folder directives

---
+++ app/directives/speciality_validate.directive.js
@@ -0,0 +1,63 @@
+(function() {
+    "use strict";
+
+    angular.module("app.admin")
+        .directive("validateDirective", validateDirective);
+
+    validateDirective.$inject = ["SPECIALITIES_CONST", "specialitiesService"];
+
+    function validateDirective(SPECIALITIES_CONST, specialitiesService) {
+        var all;
+        getSpecialities();
+
+        function getSpecialities(){
+            specialitiesService.getSpecialities().then(function(data) {
+               all = data;
+            });
+        }
+
+        function validText(regexp, text) {
+
+            return regexp.test(text);
+        }
+
+        function alreadyExist(arrObj, key, text){
+            for (var i = 0; i < arrObj.length; i++) {
+                if (arrObj[i][key] === text) {
+
+                    return true;
+                };
+            };
+
+            return false;
+        }
+
+        function whichInput(inputName){
+            var attr = {};
+            if (inputName === "specialityName"){
+                attr.regexp = SPECIALITIES_CONST.NAME_REGEXP;
+                attr.key = "speciality_name";
+            } else if (inputName === "specialityCode"){
+                attr.regexp = SPECIALITIES_CONST.CODE_REGEXP;
+                attr.key = "speciality_code";
+            }
+
+            return attr;
+        }
+
+        return {
+            restrict: "EA",
+            require: "ngModel",
+            link: function(scope, element, attr, mCtrl) {
+                function validation(value) {
+                    mCtrl.$setValidity("validText", validText(whichInput(attr.name).regexp, value));
+                    mCtrl.$setValidity("alreadyExist", !alreadyExist(all, whichInput(attr.name).key, value));
+
+                    return value;
+                }
+                mCtrl.$parsers.push(validation);
+            }
+        };
+     };
+
+})();
